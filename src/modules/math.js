9a3a425273deabf569006a2dab107e4a018db47e
Ashley Marques <marques.ashley@gmail.com>
2021-11-29 20:29:37 -0300

    math unit testing

--- src/modules/math.js
+++ src/modules/math.js
@@ -1,13 +1,54 @@
-export const name = 'square';
+const round = (value) => {
+  return Math.round(value * 100) / 100;
+};
 
-export function draw(ctx, length, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, length, length);
+const byMonthRate = (annualTax) => {
+  if (isNaN(annualTax)) {
+    return 0;
+  }
+  const aTax = +annualTax;
+  if (aTax < 0) {
+    return 0;
+  }
+  const _tax = round(aTax / 12);
+  return _tax;
+};
 
-  return {
-    length: length,
-    x: x,
-    y: y,
-    color: color,
-  };
-}
+const principalInterest = (interestRate, loanAmount, yearsOfMortgage) => {
+  if (isNaN(interestRate)) {
+    return 0;
+  }
+  interestRate = +interestRate;
+  if (interestRate <= 0) {
+    return 0;
+  }
+  if (isNaN(loanAmount)) {
+    return 0;
+  }
+  loanAmount = +loanAmount;
+  if (loanAmount <= 0) {
+    return 0;
+  }
+  if (isNaN(yearsOfMortgage)) {
+    return 0;
+  }
+  yearsOfMortgage = +yearsOfMortgage;
+  if (yearsOfMortgage <= 0) {
+    return 0;
+  }
+  const ans = round(
+    ((interestRate / 100 / 12) * loanAmount) /
+      (1 - Math.pow(1 + interestRate / 100 / 12, -yearsOfMortgage * 12))
+  );
+  return ans;
+};
+
+const tax = byMonthRate;
+
+const insurance = byMonthRate;
+
+module.exports = {
+  tax: tax,
+  insurance: insurance,
+  principalInterest: principalInterest,
+};
---
+++ src/modules/math.test.js
@@ -0,0 +1,94 @@
+const { principalInterest } = require('./math');
+const math = require('./math');
+
+describe('Tax function tests', () => {
+  test('Should be 100 when annualTax is 1200', () => {
+    expect(math.tax(1200)).toBe(100);
+  });
+
+  test('Should be 40 when annualTax is 480', () => {
+    expect(math.tax(480)).toBe(40);
+  });
+  test('Should be 0 when annualTax is negative number (-120)', () => {
+    expect(math.tax(-120)).toBe(0);
+  });
+
+  test('Should be 0 when annualTax is 0', () => {
+    expect(math.tax(0)).toBe(0);
+  });
+
+  test('Should be 0 when annualTax is NaN ("1d000")', () => {
+    expect(math.tax('1d000')).toBe(0);
+  });
+  test('Should be 83.33 when annualTax is 1000', () => {
+    expect(math.tax(1000)).toBe(83.33);
+  });
+});
+
+describe('Insurance function tests', () => {
+  test('Should be 100 when annualInsurance is 1200', () => {
+    expect(math.insurance(1200)).toBe(100);
+  });
+
+  test('Should be 40 when annualInsurance is 480', () => {
+    expect(math.insurance(480)).toBe(40);
+  });
+  test('Should be 0 when annualInsurance is negative number (-120)', () => {
+    expect(math.insurance(-120)).toBe(0);
+  });
+  test('Should be 0 when annualInsurance is 0', () => {
+    expect(math.insurance(0)).toBe(0);
+  });
+
+  test('Should be 0 when annualInsurance is NaN ("1d000")', () => {
+    expect(math.insurance('1d000')).toBe(0);
+  });
+  test('Should be 25.0 when annualInsurance is 300', () => {
+    expect(math.insurance(300)).toBe(25.0);
+  });
+  test('Should be 8.33 when annualInsurance is 100', () => {
+    expect(math.insurance(100)).toBe(8.33);
+  });
+});
+
+describe('Principle & Interest tests', () => {
+  test('Should return 0 when interestRate is NaN', () => {
+    expect(math.principalInterest('5t5', 100, 20)).toBe(0);
+  });
+  test('Should return 0 when interestRate is a negative number', () => {
+    expect(math.principalInterest(-5, 100, 20)).toBe(0);
+  });
+  test('Should return 0 when interestRate is equals to 0', () => {
+    expect(math.principalInterest(0, 100, 20)).toBe(0);
+  });
+  test('Should return 0 when loanAmount is NaN', () => {
+    expect(math.principalInterest(1.8, '2y00', 20)).toBe(0);
+  });
+  test('Should return 0 when loanAmount is a negative number', () => {
+    expect(math.principalInterest(1.8, -25000, 20)).toBe(0);
+  });
+  test('Should return 0 when loanAmount is equals to 0', () => {
+    expect(math.principalInterest(1.8, 0, 20)).toBe(0);
+  });
+  test('Should return 0 when yearsOfMortgage is NaN', () => {
+    expect(math.principalInterest(1.8, 400000, '3r0')).toBe(0);
+  });
+  test('Should return 0 when yearsOfMortgage is a negative number', () => {
+    expect(math.principalInterest(1.8, 400000, -30)).toBe(0);
+  });
+  test('Should return 0 when yearsOfMortgage is equals to 0', () => {
+    expect(math.principalInterest(1.8, 400000, 0)).toBe(0);
+  });
+  test('Should return 1438.79 when interestRate=1.8 loanAmount=400K yearsOfMortage=30', () => {
+    expect(math.principalInterest(1.8, 400000, 30)).toBe(1438.79);
+  });
+});
+
+describe('Monthly payment', () => {
+  test('Should be 1547.12 when interestRate=1.8 loanAmount=400K yearsOfMortage=30 annualTax=1000 annualInsurance=300', () => {
+    const interest = math.principalInterest(1.8, 400000, 30);
+    const tax = math.tax(1000);
+    const insurance = math.insurance(300);
+    expect(interest + tax + insurance).toBe(1547.12);
+  });
+});
